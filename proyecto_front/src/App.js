6265d0dbc8fac83b7a03fb9685a66eda3088d10c
Luis Aruquipa Callata <laruquipa@unsa.edu.pe>
2021-05-04 08:28:30 -0500

    avance1

--- proyecto_front/src/App.js
+++ proyecto_front/src/App.js
@@ -1,54 +1,34 @@
 import React from 'react';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
-const App = () => {
+
+function App(){
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">ACStore</a>
-        </div>
-        <div>
-          <a href="/cart">Carrito</a>
-          <a href="/signin">Iniciar Sesión</a>
-        </div>
-      </header>
-      <main>
-        <div>
-        <div className="row center">
-          {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/@{product._id}`}>
 
-                  <img className="medium" src={product.image} alt={product.name} />
-                </a>
-                <div className="card-body">
-                <a href={`/product/@{product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span> <i className="fa fa-star"></i></span>
-                    <span> <i className="fa fa-star"></i></span>
-                    <span> <i className="fa fa-star"></i></span>
-                    <span> <i className="fa fa-star"></i></span>
-                  </div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
+    <BrowserRouter>
 
-            ))
-              }
-              </div>
-        </div>
-
-
-
-      </main>
-      <footer className=" row center">
-      Todos los derechos reservados
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">ACStore</a>
+          </div>
+          <div>
+            <a href="/cart">Carrito</a>
+            <a href="/signin">Iniciar Sesión</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className=" row center">
+          Todos los derechos reservados
         </footer>
-    </div >
+      </div >
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
